c6b951de1d591f6b3fa98921b48012349958351e
Timothy Davenport <tmthydvnprt@users.noreply.github.com>
2015-05-17 10:53:00 -0700

    quilt the output

---
+++ output/js/resume.js
@@ -0,0 +1,208 @@
+/*globals $,microformat,timContact,timeDuration */
+
+var timResume = $.extend({}, microformat["h-resume"], {
+    "p-name"             : "Engineer",
+    "p-summary"          : "<s>An electrical engineer turned software developer (the most interesting techniques cannot be done in hardware). Loves high-level languages & algorithms that allow for development at an information / data level that is mostly abstracted from it's processing implementation. (enter python, stage right, playing the part of executable psuedo-code).</s>",
+    "p-contact"          : timContact,
+    "p-education"        : [
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "Master of Science in Electrical Engineering",
+            "p-abbr"             : "M.Sc. in E.E.",
+            "p-summary"          : "Image Processing",
+            "dt-start"           : "2011-09",
+            "dt-end"             : "2012-06",
+            "dt-duration"        : timeDuration("2011-09", "2012-06"),
+            "p-location"         : "California Polytechnic State University, San Luis Obispo California"
+        }),
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "Bachelor of Science in Electrical Engineering",
+            "p-abbr"             : "B.Sc. in E.E.",
+            "p-summary"          : "Digital Signal Processing",
+            "dt-start"           : "2007-08",
+            "dt-end"             : "2012-06",
+            "dt-duration"        : timeDuration("2007-08", "2012-06"),
+            "p-location"         : "California Polytechnic State University, San Luis Obispo California"
+        }),
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "High School Diploma",
+            "p-abbr"             : "H.S.",
+            "p-summary"          : "A.P. Math and Science",
+            "dt-start"           : "2003-08",
+            "dt-end"             : "2007-06",
+            "dt-duration"        : timeDuration("2003-08", "2007-06"),
+            "p-location"         : "Foothill Technology High School, Ventura California"
+        })
+    ],
+    "p-experience"       : [
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "Electrical Engineer II",
+            "p-abbr"             : "EE II",
+            "dt-start"           : "2012-10",
+            "dt-end"             : "present",
+            "dt-duration"        : timeDuration("2012-10", "present"),
+            "p-category"         : "Full Time",
+            "p-description"      : 'Hardware & Firmware design for Digital Signal Processing in <abbr title="Electronic Warfare">EW</abbr> applications',
+            "p-location"         : 'Raytheon <abbr title="Space and Airborne Systems">SAS</abbr>'
+        }),
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "Electrical Engineering Intern",
+            "p-abbr"             : "EE Intern",
+            "dt-start"           : "2009-06",
+            "dt-end"             : "2011-09",
+            "dt-duration"        : timeDuration("2009-06", "2011-09"),
+            "p-category"         : "Full Time Summer Internship",
+            "p-description"      : 'Hardware & Firmware design for Digital Signal Processing in <abbr title="Electronic Warfare">EW</abbr> applications',
+            "p-location"         : 'Raytheon <abbr title="Space and Airborne Systems">SAS</abbr>'
+        }),
+        /*
+            "dt-start"           : "2010-07",
+            "dt-end"             : "2010-09",
+            "dt-start"           : "2010-01",
+            "dt-end"             : "2010-03",
+            "dt-start"           : "2009-06",
+            "dt-end"             : "2009-08",
+        */
+        $.extend({}, microformat["h-event"], {
+            "p-name"             : "Barista",
+            "p-abbr"             : "Barista",
+            "dt-start"           : "2005-05",
+            "dt-end"             : "2008-12",
+            "dt-duration"        : timeDuration("2005-05", "2008-12"),
+            "p-category"         : "Part Time",
+            "p-description"      : "Shift Lead managing a coffeehouse",
+            "p-location"         : "Simone's Coffee & Tea"
+        })
+    ],
+    "p-skill"           : [
+        $.extend({}, microformat["h-skill"], {
+            "p-name"             : "Languages Spoken",
+            "p-summary"          : "I am a polyglot",
+            "p-skillset"         : [
+                $.extend({}, microformat["h-skillset"], {
+                    "p-name"         : "Natural",
+                    "p-competency"   : [
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "English",
+                            "p-abbr"             : "English",
+                            "p-rating"           : "5",
+                            "dt-start"           : "1990-01",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("1990-01", "present")
+                        })
+                    ]
+                }),
+                $.extend({}, microformat["h-skillset"], {
+                    "p-name"         : "Computational",
+                    "p-competency"   : [
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "Python",
+                            "p-abbr"             : "Python",
+                            "p-rating"           : "5",
+                            "dt-start"           : "2011-04",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2011-04", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "JavaScript",
+                            "p-abbr"             : "JavaScript",
+                            "p-rating"           : "4",
+                            "dt-start"           : "2006-09",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2006-09", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "MATLAB",
+                            "p-abbr"             : "MATLAB",
+                            "p-rating"           : "5",
+                            "dt-start"           : "2011-01",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2011-01", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "VHDL",
+                            "p-abbr"             : "VHDL",
+                            "p-rating"           : "5",
+                            "dt-start"           : "2008-09",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2008-09", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "PHP",
+                            "p-abbr"             : "PHP",
+                            "p-rating"           : "2",
+                            "dt-start"           : "2012-08",
+                            "dt-end"             : "2013-08",
+                            "dt-duration"        : timeDuration("2012-08", "2013-08")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "Bash",
+                            "p-abbr"             : "Bash",
+                            "p-rating"           : "1",
+                            "dt-start"           : "2012-10",
+                            "dt-end"             : "2013-10",
+                            "dt-duration"        : timeDuration("2012-10", "2013-10")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "Tcl, Verilog, VisualBasic, C, AWK, Sed",
+                            "p-abbr"             : "Tcl, Verilog, VisualBasic, C, AWK, Sed",
+                            "p-rating"           : "1",
+                            "dt-start"           : "2008-01",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2008-01", "present"),
+                            "p-note"             : "I do not enjoy this last group and have a limited understanding, but I can hack something together if need be."
+                        })
+                    ],
+                    "p-note" : "Ordered by enjoyment; rated by understanding."
+                }),
+                $.extend({}, microformat["h-skillset"], {
+                    "p-name"         : "Presentational",
+                    "p-competency"   : [
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "HTML",
+                            "p-abbr"             : "HTML",
+                            "p-rating"           : "5",
+                            "dt-start"           : "2005-09",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2005-09", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "CSS",
+                            "p-abbr"             : "CSS",
+                            "p-rating"           : "4",
+                            "dt-start"           : "2006-01",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2006-01", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "Markdown",
+                            "p-abbr"             : "Markdown",
+                            "p-rating"           : "5",
+                            "dt-start"           : "2013-01",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2013-01", "present")
+                        }),
+                        $.extend({}, microformat["h-competency"], {
+                            "p-summary"          : "LaTeX",
+                            "p-abbr"             : "LaTeX",
+                            "p-rating"           : "1",
+                            "dt-start"           : "2012-05",
+                            "dt-end"             : "present",
+                            "dt-duration"        : timeDuration("2012-05", "present")
+                        })
+                    ]
+                })
+            ]
+        })
+    ],
+    "p-publication" : [
+        $.extend({}, microformat["h-cite"], {
+            "p-publication"      : "Master's Thesis",
+            "p-name"             : "Early Forest Fire Detection using Texture Analysis of Principal Components from Multispectral Video",
+            "dt-published"       : "2012-06",
+            "p-author"           : "Timothy M. Davenport",
+            "u-url"              : "http://digitalcommons.calpoly.edu/theses/795/",
+            "p-content"          : "[Abstract] The aim of this study is to incorporate the spectral, temporal and spatial attributes of a smoke plume for Early Forest Fire Detection. Image processing techniques are used on multispectral (red, green, blue, mid-wave infrared, and long-wave infrared) video to segment and indentify the presence of a smoke plume within a scene. <a href=\"http://digitalcommons.calpoly.edu/theses/795/#abstract\">...</a>"
+        })
+    ],
+    "p-note" : 'This r&eacute;sum&eacute; is a <a href=\"#!/jsonresume\">json object</a> containing <a href=\"http://microformats.org/wiki/microformats2\">microformat2</a> structured data and rendered as html.  The semantic data uses the draft vocabularies <code>h-resume</code>, <code>h-card</code>, <code>h-adr</code>, <code>h-geo</code> and <code>h-event</code>, along with experiemental vocabularies <code>h-skill</code>, <code>h-skillset</code>, <code>h-competency</code> and <code>h-cite</code>.  Artistic license was exercised by using non-standard custom properties (e.g. <code>dt-start</code>/<code>end</code> in <code>h-compentency</code>, or <code>p-note</code> in any <code>h</code> vocab); however these were, hopefully, used as logical extentions of the official <abbr title="Application Programming Interface">API</abbr> and shouldn\'t break a microformat2 parser. <a href="#!/contact">Contact</a> me if you have suggestions.'
+});
---
+++ output/js/tim.js
@@ -0,0 +1,352 @@
+/*globals $,microformat,timeDuration,renderMicroFormat,ganttResume,timResume,timContact,console,prettyPrint*/
+
+$(document).ready(function () {
+
+/*
+	var pithies = [
+		'Sometimes I think orchestras rock out harder than metal heads.',
+		'I would alter Freud\'s statement to say "to love and to learn"',
+		'This is some pithy quote that would sum up myself & this whole website.',
+		'Unquestionably, one ought never be board, for there are far to great a number of things to fascinate and occupy the mind.',
+		'What you are about to experience may or may not make sense; either you <em>will</em> understand, or you will have been given <strong>strings</strong>, threads to unravel a spool of endless topics <em>to</em> understand.'
+	];
+*/
+
+	// page js
+	// -------------------------------------------------------------------------------------
+	var page              = $('#page'),
+		second            = $('#second'),
+		minute            = $('#minute'),
+		hour              = $('#hour'),
+		localtime         = $('#localtime'),
+		bigSecond         = {},
+		bigMinute         = {},
+		bigHour           = {},
+		bigWord           = {},
+		local             = {},
+		utc               = {},
+		julian            = {},
+		posix             = {},
+		hash              = '',
+        urlpath           = '',
+        filename          = '',
+        newlocation       = '',
+		alphabet          = '`1234567890-=qwertyuiop[]\\asdfghjkl;\'zxcvbnm,./~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:\"ZXCVBNM<>?',
+		verbInterval      = 0,
+		charInterval      = 0,
+		secondInterval    = 0,
+		secondBigInterval = 0,
+		initHoldOff       = 0,
+		bringOut          = 0,
+		pageOrder         = ['home', 'epigraph', 'synopsis', 'ruminations', 'resume', 'colophon', 'contact'],
+		currentPage       = 0,
+		geometers         = '<h1 class="geometers">&Alpha;&Gamma;&Epsilon;&Omega;&Mu;&Epsilon;&Tau;&Rho;&Eta;&Tau;&Omicron;&Sigma; &Mu;&Eta;&Delta;&Epsilon;&Iota;&Sigma; &Epsilon;&Iota;&Sigma;&Iota;&Tau;&Omega;</h1>';
+
+    
+	// General Functions
+	// -------------------------------------------------------------------------------------
+	function wordTime(time) {
+		var hour      = time.getHours(),
+			minute    = time.getMinutes(),
+			second    = time.getSeconds(),
+			ones      = [null, 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+			teens     = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+			tens      = [null, null, 'twenty', 'thirty', 'forty', 'fifty'],
+			hourStr   = '',
+			minuteStr = '',
+			secondStr = '';
+		if (second === 0) {
+			secondStr = '&nbsp;';
+		} else if (second < 10) {
+			secondStr = 'o\'' + ones[second];
+		} else if (second < 19) {
+			secondStr = teens[second - 10];
+		} else {
+			secondStr  = tens[Math.floor(second / 10)];
+			secondStr += (second % 10 === 0) ? '' : '-' + ones[second % 10];
+		}
+		if (minute === 0) {
+			minuteStr = 'o\'clock';
+		} else if (minute < 10) {
+			minuteStr = 'o\'' + ones[minute];
+		} else if (minute < 19) {
+			minuteStr = teens[minute - 10];
+		} else {
+			minuteStr  = tens[Math.floor(minute / 10)];
+			minuteStr += (minute % 10 === 0) ? '' : '-' + ones[minute % 10];
+		}
+		hourStr = ones.concat(teens)[hour % 12];
+		return '<p>' + hourStr + '</p><p>' + minuteStr + '</p><p>' + secondStr + '</p>';
+	}
+
+	function tickClock() {
+		var now    = new Date(),
+			hrDeg  = 180 + Math.floor(360 * (now.getHours()   / 12)) % 360,
+			minDeg = 180 + Math.floor(360 * (now.getMinutes() / 60)),
+			secDeg = 180 + Math.floor(360 * (now.getSeconds() / 60));
+		second.css('-webkit-transform', 'rotate(' + secDeg + 'deg)');
+		minute.css('-webkit-transform', 'rotate(' + minDeg + 'deg)');
+		hour.css('-webkit-transform', 'rotate(' + hrDeg + 'deg)');
+		localtime.html('<h4><span class="time">' + now.toLocaleTimeString().split('').join('</span><span class="time">') + '</span> <small><span class="time">' + now.toLocaleDateString().split('').join('</span><span class="time">') + '</span></small></h4>');
+		return false;
+	}
+	function tickBigClock() {
+		var now    = new Date(),
+			hrDeg  = 180 + Math.floor(360 * (now.getHours()   / 12)) % 360,
+			minDeg = 180 + Math.floor(360 * (now.getMinutes() / 60)),
+			secDeg = 180 + Math.floor(360 * (now.getSeconds() / 60)),
+            isoString  = now.getUTCFullYear() + '-' + (now.getUTCMonth() + 1) + '-' + now.getUTCDate() + 'T' + now.getUTCHours() + ':' + now.getUTCMinutes() + ':' + now.getUTCSeconds();
+
+		bigSecond.css('-webkit-transform', 'rotate( ' + secDeg + 'deg)');
+		bigMinute.css('-webkit-transform', 'rotate( ' + minDeg + 'deg)');
+		bigHour.css('-webkit-transform', 'rotate(' + hrDeg + 'deg)');
+
+		bigWord.html(wordTime(now));
+		local.text(now.toLocaleString());
+		utc.text(now.toUTCString());
+		posix.text((+Math.floor(now / 1000)));
+		julian.text(((now / 86400000) + 2440587.5));
+
+		bigWord.attr('datetime', isoString);
+		local.attr('datetime', isoString);
+		utc.attr('datetime', isoString);
+		posix.attr('datetime', isoString);
+		julian.attr('datetime', isoString);
+
+		return false;
+	}
+	function renderTemplate(name, titleText) {
+		if (titleText) {
+			$('title').html(titleText);
+		} else {
+			$('title').text(name);
+		}
+		page.html($('#' + name + '-template').html());
+		$(page.children()[0]).attr('id', name);
+	}
+	function backNextChange(url) {
+		if (url === 'back') {
+			currentPage = (currentPage > 0) ? (currentPage - 1) : 0;
+			window.location = window.location.href.replace(window.location.hash, '') + '#!/' + pageOrder[currentPage];
+		} else if (url === 'next') {
+			currentPage = (currentPage < pageOrder.length - 1) ? (currentPage + 1) : pageOrder.length - 1;
+			window.location = window.location.href.replace(window.location.hash, '') + '#!/' + pageOrder[currentPage];
+		}
+	}
+
+	// store pages
+	var pages = {
+		home     : function () {
+			renderTemplate('home', 'tim(othy)');
+
+			var verb           = $('#verb'),
+				verbs          = ['read', 'code', 'write', 'walk', 'ponder', 'hike', 'garden'],
+				verbIndx       = 0,
+				maxVerbLen     = 0,
+				verbText       = '',
+				r              = '',
+				charPeriod     = 40,
+				verbPeriod     = 2500,
+				randCharNum    = 5,
+				randChar       = 0,
+				charIndx       = 0;
+			verbs.forEach(function (item) {
+				maxVerbLen = Math.max(maxVerbLen, item.length);
+			});
+
+			// timing intervals
+			verbInterval = setInterval(function () {
+				verbText = new Array(maxVerbLen + 1).join('_');
+				charInterval = setInterval(function () {
+					if (charIndx < verbs[verbIndx].length) {
+						if (randChar < randCharNum) {
+							r        = Math.floor(alphabet.length * Math.random());
+							verbText = verbText.substr(0, charIndx) + alphabet[r] + verbText.substr(charIndx + 1);
+							randChar += 1;
+						} else {
+							verbText = verbText.substr(0, charIndx) + verbs[verbIndx][charIndx] + verbText.substr(charIndx + 1);
+							charIndx += 1;
+							randChar = 0;
+						}
+						verb.text(verbText);
+					} else {
+						verbText = verbs[verbIndx] + verbText.substr(verbs[verbIndx].length);
+						verbIndx = (verbIndx + 1) % verbs.length;
+						randChar  = 0;
+						charIndx  = 0;
+						verb.text(verbText);
+						clearInterval(charInterval);
+					}
+				}, charPeriod);
+			}, verbPeriod);
+		},
+		epigraph    : function () {
+			renderTemplate('epigraph', 'tim(othy) > epigraph');
+		},
+		synopsis    : function () {
+			renderTemplate('synopsis', 'tim(othy) > synopsis');
+		},
+		ruminations : function () {
+			renderTemplate('ruminations', 'tim(othy) > ruminations');
+		},
+		resume      : function () {
+			renderTemplate('resume', 'tim(othy) > r&eacute;sum&eacute;');
+			$('#tim-resume').html(renderMicroFormat(timResume, $('#h-resume-template').html()));
+			$('#tim-resume').append('<hr><h1 class="text-center"><em>Timeline</em></h1>');
+			$('#tim-resume').append(ganttResume(timResume));
+			$('#tim-resume').append(geometers);
+		},
+		jsonresume  : function () {
+			renderTemplate('jsonresume', 'tim(othy) > r&eacute;sum&eacute; > (json + microformat2)');
+			$('#tim-jsonresume').text(JSON.stringify(timResume, undefined, '  '));
+			$('#tim-jsonresume').append(geometers);
+			prettyPrint();
+		},
+		contact     : function () {
+			renderTemplate('contact', 'tim(othy) > contact');
+			$('#tim-contact').html(renderMicroFormat(timContact, $('#h-card-template').html()));
+			$('#tim-contact .p-note').remove();
+		},
+		colophon    : function () {
+			renderTemplate('colophon', 'tim(othy) > colophon');
+
+			$('.typo').hover(function () {
+				$('#lorem h1').text($(this).css('font-family').split(', ')[0].replace(/'/g, ''));
+				$('#lorem').css({
+					'font-family' : $(this).css('font-family'),
+					'top'        : window.pageYOffset
+				});
+			}, function () {
+				$('#lorem').css({
+					'top'        : '-1000px'
+				});
+			});
+
+		},
+		monogram   : function () {
+			renderTemplate('monogram', 'tim(othy) > monogram');
+		},
+		fontpy    : function () {
+			renderTemplate('fontpy', 'tim(othy) > font-size python script');
+			prettyPrint();
+		},
+		base64imagepy : function () {
+			renderTemplate('base64imagepy', 'tim(othy) > base64 image python script');
+			prettyPrint();
+		},
+		time        : function () {
+			renderTemplate('time', 'timo(othy) > time');
+			$('#clock').addClass('holdoff-time');
+			bigSecond = $('#bigsecond');
+			bigMinute = $('#bigminute');
+			bigHour   = $('#bighour');
+			bigWord   = $('#bigword');
+			local     = $('#local');
+			utc       = $('#utc');
+			julian    = $('#julian');
+			posix     = $('#posix');
+			tickBigClock();
+			secondBigInterval = setInterval(function () {
+				tickBigClock();
+			}, 1000);
+		}
+	};
+
+	// route hashchanges to page
+	function router() {
+
+		// clear last page stuff
+		clearInterval(charInterval);
+		clearInterval(verbInterval);
+		clearInterval(secondBigInterval);
+		$('.holdoff-time').removeClass('holdoff-time');
+
+		// cache page, hash, and filename
+		page = page || $('#page');
+		hash = location.hash.slice(1);
+        urlpath = location.pathname.split('/');
+        filename = urlpath.slice(-1)[0];
+        
+        console.log('page ' + page);
+        console.log('hash ' + hash);
+        console.log('urlpath ' + urlpath);
+        console.log('filename ' + filename);
+        
+        // off-page hash
+        if (filename === 'index.html') {
+            // default to home if one index page
+            hash = hash || '!/home';
+            
+            if (hash.slice(0, 2) === '!/') {
+                
+                $('#page section').addClass('bringOut');
+
+                bringOut = setTimeout(function () {
+                    page.removeClass('rendered');
+                    page.addClass('routing');
+
+                    hash = hash.slice(2);
+                    if (pages.hasOwnProperty(hash)) {
+                        currentPage = pageOrder.indexOf(hash);
+                        pages[hash]();
+                    } else {
+                        currentPage = 0;
+                        pages.home();
+                    }
+
+                    // setup
+                    page.addClass('rendered');
+
+                    $('html,body').animate({
+                        scrollTop: 0
+                    }, 512);
+
+                    clearInterval(bringOut);
+                }, 500);
+
+            // on-page hash = scroll to any on page target
+            } else if (hash) {
+                if (hash === 'back' || hash === 'next') {
+                    backNextChange(hash);
+                } else {
+                    var target = $(this.hash);
+                    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                    if (target.length) {
+                        $('html,body').animate({
+                            scrollTop: target.offset().top - 60
+                        }, 512);
+                    }
+                }
+            }
+        } else {
+            if (hash.slice(0, 2) === '!/') {
+                urlpath[urlpath.length - 1] = "index.html";
+                newlocation = urlpath.join("/") + location.hash;
+                location.assign(newlocation);
+            }
+        }
+	}
+
+	initHoldOff = setTimeout(function () {
+		$('.holdoff').removeClass('holdoff');
+		clearTimeout(initHoldOff);
+	}, 256);
+
+	// listen for hash change or page load
+	$(window).on('hashchange', router);
+	$(window).on('load', router);
+
+	// listen for key up change
+	$(window).keyup(function (e) {
+		if (e.keyCode === 37) {
+			backNextChange('back');
+		} else if (e.keyCode === 39) {
+			backNextChange('next');
+		}
+	});
+
+	tickClock();
+	secondInterval = setInterval(function () {
+		tickClock();
+	}, 1000);
+});
